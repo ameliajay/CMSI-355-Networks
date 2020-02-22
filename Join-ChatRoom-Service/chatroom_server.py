559d67e32dfb18a4e089408edbc219dc15e01bd6
Amelia Jay <31746937+ameliajay@users.noreply.github.com>
2020-02-22 15:29:21 -0800

    updated joining and leaving chatroom service

--- Join-ChatRoom-Service/chatroom_server.py
+++ Join-ChatRoom-Service/chatroom_server.py
@@ -7,25 +7,42 @@ class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 
 class UserHandler(socketserver.StreamRequestHandler):
     room = []
-    # this can become a chatroom class
+    users = {}
+    # need to add a chatroom class
     def handle(self):
         client = f'{self.client_address} on {threading.currentThread().getName()}'
         print(f'Connected: {client}')
         self.send('Welcome to The Chat Room\n')
-        self.send('Please submit your name\n')
+        self.send('Please submit a username - letters and numbers only please\n')
         while True:
             name = self.rfile.readline().decode('utf-8').rstrip('\n')
+            # do some other checks here to make sure name isn't just a bunch of spaces
             if not name:
-                self.send('You must submit a name to continue\n')
-                return
+                self.send('You must submit a username to continue\n')
+            elif name in self.room:
+                self.send('That username is already taken - please submit another\n')
             else:
                 self.room.append(name)
-                self.send('Someone new has entered The Chat Room')
-                self.send(self.room)
+                self.users[client] = name
+                # I want this to be sent to all the users, not just printed
+                print('The Chat Room Members are:')
+                for user in self.room:
+                    print(user)
                 break
+        # the below while loop is not the messenging service but is just a placeholder
+        # the messenging part of the service is in the other folder
+        while True:
+            message = self.rfile.readline().decode('utf-8')
+            if not message:
+                break
+            self.send(message)
+        print(f'Closed: {client}')
+        self.room.remove(self.users[client])
+        print(f'{name} has left The Chat Room\n')
+        print(f'Users still in The Chat Room: {self.room}\n')
                 
-def send(self, message):
-    self.wfile.write(f'{message}\n'.encode('utf-8'))
+    def send(self, message):
+        self.wfile.write(f'{message}\n'.encode('utf-8'))
     
 with ThreadedTCPServer(('', 59898), UserHandler) as server:
     print(f'The Chat Room is live...')
